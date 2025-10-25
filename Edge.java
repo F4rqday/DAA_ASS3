482bc225e7f76282c957d75d95559849651b9d72
Alt+F₄raday <112538706+F4rqday@users.noreply.github.com>
2025-10-25 21:40:46 +0600

    Add Edge and Graph classes for MST

---
+++ Edge.java
@@ -0,0 +1,15 @@
+public class Edge implements Comparable<Edge> {
+    String from, to;
+    int weight;
+
+    public Edge(String from, String to, int weight) {
+        this.from = from;
+        this.to = to;
+        this.weight = weight;
+    }
+
+    @Override
+    public int compareTo(Edge other) {
+        return Integer.compare(this.weight, other.weight);
+    }
+}
